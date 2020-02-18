348f6303d7e9062162ddf2a696620277fc9556b4
littlefxc <12741723+littlefxc@users.noreply.github.com>
2020-02-18 13:34:17 +0800

    docs(seed-security-app):重构注册逻辑

--- seed-security-app/src/main/java/com/fengxuechao/seed/security/app/social/SpringSocialConfigurerPostProcessor.java
+++ seed-security-app/src/main/java/com/fengxuechao/seed/security/app/social/SpringSocialConfigurerPostProcessor.java
@@ -1,6 +1,8 @@
 package com.fengxuechao.seed.security.app.social;
 
+import com.fengxuechao.seed.security.app.AppSecurityController;
 import com.fengxuechao.seed.security.properties.SecurityConstants;
+import com.fengxuechao.seed.security.social.SocialConfig;
 import com.fengxuechao.seed.security.social.support.SeedSpringSocialConfigurer;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeansException;
@@ -8,25 +10,38 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
 /**
+ * BeanPostProcessor的作用：在 Spring 容器初始化之前和之后都要经过这个接口定义的两个方法
+ *
  * @author fengxuechao
  * @date 2020-02-09
  */
 @Component
 public class SpringSocialConfigurerPostProcessor implements BeanPostProcessor {
 
-	@Override
-	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-		return bean;
-	}
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
 
-	@Override
-	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-		if(StringUtils.equals(beanName, "seedSocialSecurityConfig")){
-			SeedSpringSocialConfigurer config = (SeedSpringSocialConfigurer)bean;
-			config.signupUrl(SecurityConstants.DEFAULT_SOCIAL_USER_INFO_URL);
-			return config;
-		}
-		return bean;
-	}
+    /**
+     * Spring 容器初始化 SeedSpringSocialConfigurer 之后改变 signUp 地址
+     *
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     * @see SeedSpringSocialConfigurer
+     * @see SocialConfig#seedSocialSecurityConfig()
+     * @see AppSecurityController#getSocialUserInfo(javax.servlet.http.HttpServletRequest)
+     */
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (StringUtils.equals(beanName, "seedSocialSecurityConfig")) {
+            SeedSpringSocialConfigurer config = (SeedSpringSocialConfigurer) bean;
+            config.signupUrl(SecurityConstants.DEFAULT_SOCIAL_USER_INFO_URL);
+            return config;
+        }
+        return bean;
+    }
 
 }
